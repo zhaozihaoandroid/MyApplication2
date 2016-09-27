9127705bb489d24e3d843537fe2056cf32363881
zhaozihao <519284901@qq.com>
2016-09-27 10:56:40 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/zhaozihao/myapplication/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.zhaozihao.myapplication;
+
+
+import android.app.Activity;
+
+/**
+ * Created by zhaozihao on 16/9/27.
+ */
+
+public class SecondActivity extends Activity{
+
+
+
+}
+
+
