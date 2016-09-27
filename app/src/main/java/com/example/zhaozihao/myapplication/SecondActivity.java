0e4d2f3c93348e0352722176e03b0aff0a64a054
zhaozihao <519284901@qq.com>
2016-09-27 15:51:43 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/zhaozihao/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/zhaozihao/myapplication/SecondActivity.java
@@ -2,15 +2,18 @@ package com.example.zhaozihao.myapplication;
 
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by zhaozihao on 16/9/27.
  */
 
 public class SecondActivity extends Activity{
-
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //aa
+    }
 }
 
 
